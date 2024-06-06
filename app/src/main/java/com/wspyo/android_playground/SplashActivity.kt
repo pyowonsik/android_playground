77da591c26cc84f9331a121e365c655f4deb55d2
pyo_16 <94816219+pyowonsik@users.noreply.github.com>
2024-06-06 17:34:38 +0900

    feat : splash

---
+++ app/src/main/java/com/wspyo/android_playground/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.wspyo.android_playground
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // splash 처리를 위해 3초후 main activity로 이동
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
